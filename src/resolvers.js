662d63fa98fa749f230062573edccb83f1eb4b62
Ashik Mahmud <ashikmahmud@Ashiks-MacBook-Pro.local>
2021-06-16 12:36:54 +0600

    Code refactored

---
+++ src/resolvers.js
@@ -0,0 +1,64 @@
+const {createUserId} = require("./utils");
+
+const newUserId = createUserId();
+
+const {users} = require("./usersData");
+
+const resolvers = {
+    Query: {
+        helloworld: () => 'hello world! what a day!',
+        users: (parent, args, context, info) => {
+            if(args.text){
+                return users.filter((elem) => elem.firstName.toLowerCase().includes(args.text.toLowerCase()));
+            }
+            return users;
+        },
+        user: (parent, args, context, info) => {
+            console.log(args);
+            return users.find((user) => {
+                if(user.id == args.userId){
+                    return user;
+                }
+            })
+        }
+    },
+    Mutation: {
+        createUser: (parent, args, context, info) => {
+            const newUser = {
+                id: newUserId(),
+                firstName: args.firstName,
+                email: args.email,
+                age: args.age
+            }
+            const userAlreadyExist = users.some((elem) => {
+                return elem.email == args.email
+            })
+            if(userAlreadyExist){
+                throw new Error("User already exists")
+            } else{
+                users.push(newUser);
+                return newUser;
+            }
+        }
+    },
+    User: {
+        id: (parent) => parent.id,
+        firstName: (parent) => {
+            console.log('what is the parent: ', parent)
+            return parent.firstName;
+        },
+        email: (parent) => parent.email,
+        age: (parent) => {
+            console.log("age is: ", parent.age);
+            // generate random number between 1-4
+            const randomNum = Math.floor(Math.random() * 4) + 1;
+            // multiply random number with the age
+            // finally return the age
+            return parent.age * randomNum;
+        }
+    }
+}
+
+module.exports = {
+    resolvers,
+}
